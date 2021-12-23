8cf07d3338e48a60e8f98123f458936bdc077805
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-12-23 07:40:36 +0700

    96.4664.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/unxp/unx_webrtc/releases/download/exp.v2/WebRTC.xcframework.zip",
+            url: "https://github.com/unxp/unx_webrtc/releases/download/96.4664.1/WebRTC.xcframework.zip",
             checksum: "5ed7d0a94c30639a4a5b31b81addab1f0f44b10bbed122a036862fc00f6fca6b"
         ),
     ]
